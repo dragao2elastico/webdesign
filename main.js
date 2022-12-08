a25e94514449fc9af68bbb9a499eeb6571868554
dragao2elastico <arthur.santiago.silva@escola.pr.gov.br>
2022-12-08 09:05:54 -0300

    Atualizando e criando arquivos

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-// var hw =  "Hello World"
-// var divs = [
-//     "<img src=\"./img/capitaopatria.jpg\" style=\"width:100%\" onclick=\"imgExpand1()\">",
-//     "<img src=\"./img/kratos.jpg\" style=\"width:100%\" onclick=\"imgExpand2()\">",
-//     "<img src=\"./img/madara.jpg\" style=\"width:100%\" onclick=\"imgExpand3()\">"
-// ]
-// var img1 = document.getElementById("cap")
+var hw =  "Hello World"
+var divs = [
+    "<img src=\"./img/capitaopatria.jpg\" style=\"width:100%\" onclick=\"imgExpand1()\">",
+    "<img src=\"./img/kratos.jpg\" style=\"width:100%\" onclick=\"imgExpand2()\">",
+    "<img src=\"./img/madara.jpg\" style=\"width:100%\" onclick=\"imgExpand3()\">"
+]
+var img1 = document.getElementById("cap")
 
-// function imgExpand1() {
-//     img1.document.write(hw)
-// }
+function imgExpand1() {
+    img1.document.write(hw)
+}
 
-// function imgExpand2() {
+function imgExpand2() {
     
-// }
+}
 
-// function imgExpand3() {
+function imgExpand3() {
     
-// }
+}
 
 // document.onmousemove = (e) => {
 //     let body = document.querySelector('body');
